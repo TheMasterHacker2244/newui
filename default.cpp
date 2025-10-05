d74090c0877c892c7ed3c38f10442818535c213a
TheMasterHacker <93630125+TheMasterHacker2244@users.noreply.github.com>
2025-10-05 15:26:48 -0700

    Update default.cpp

--- default.cpp
+++ default.cpp
@@ -1,12 +1,6 @@
 Add-Type -Namespace Win32 -Name API -MemberDefinition @"
-[System.Runtime.InteropServices.DllImport("user32.dll", EntryPoint="LoadCursorW")]
-public static extern System.IntPtr LoadCursor(System.IntPtr h, int n);
 [System.Runtime.InteropServices.DllImport("user32.dll")]
-public static extern System.IntPtr CopyIcon(System.IntPtr h);
-[System.Runtime.InteropServices.DllImport("user32.dll")]
-public static extern bool SetSystemCursor(System.IntPtr h, uint id);
+public static extern bool SystemParametersInfo(uint action, uint param, System.IntPtr vparam, uint init);
 "@
-$arrow=[Win32.API]::LoadCursor([System.IntPtr]::Zero,32512)
-[Win32.API]::SetSystemCursor([Win32.API]::CopyIcon($arrow),32650)
-[Win32.API]::SetSystemCursor([Win32.API]::CopyIcon($arrow),32514)
-Write-Host "Loading cursors set to normal arrow."
+[Win32.API]::SystemParametersInfo(0x0057, 0, [System.IntPtr]::Zero, 0)
+Write-Host "System cursors restored to default."
