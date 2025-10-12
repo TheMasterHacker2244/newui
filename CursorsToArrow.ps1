054268866ad75a4bdeb9445ef9a365bd7f346dff
TheMasterHacker <93630125+TheMasterHacker2244@users.noreply.github.com>
2025-10-11 18:37:08 -0700

    Update CursorsToArrow.ps1

--- CursorsToArrow.ps1
+++ CursorsToArrow.ps1
@@ -6,7 +6,13 @@ public static extern System.IntPtr CopyIcon(System.IntPtr h);
 [System.Runtime.InteropServices.DllImport("user32.dll")]
 public static extern bool SetSystemCursor(System.IntPtr h, uint id);
 "@
-$arrow=[Win32.API]::LoadCursor([System.IntPtr]::Zero,32512)
-[Win32.API]::SetSystemCursor([Win32.API]::CopyIcon($arrow),32650)
-[Win32.API]::SetSystemCursor([Win32.API]::CopyIcon($arrow),32514)
-Write-Host "Loading cursors set to normal arrow."
+
+$arrow = [Win32.API]::LoadCursor([System.IntPtr]::Zero, 32512)
+
+$copy1 = [Win32.API]::CopyIcon($arrow)
+$success1 = [Win32.API]::SetSystemCursor($copy1, 32650)
+
+$copy2 = [Win32.API]::CopyIcon($arrow) 
+$success2 = [Win32.API]::SetSystemCursor($copy2, 32514)
+
+Write-Host "Loading cursors set to normal arrow. AppStarting: $success1, Wait: $success2"
