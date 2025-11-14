7df1a64ac076c7522a586bed558857683cdf7ea7
TheMasterHacker <93630125+TheMasterHacker2244@users.noreply.github.com>
2025-11-13 20:30:49 -0800

    Update DeepCleaner.ps1

--- DeepCleaner.ps1
+++ DeepCleaner.ps1
@@ -1,1 +1,2 @@
-
+Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU" -Recurse -Force
+New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU" | Out-Null
